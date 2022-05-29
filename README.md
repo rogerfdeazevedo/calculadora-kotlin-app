11a0398e12e020033ba0580baf9fa6cd35aaec9c
Roger de Azevedo <rogerfdeazevedo@gmail.com>
2022-05-29 18:09:02 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,87 @@
+[![header][header-url]][header-link]
+
+# Calculadora Kotlin App
+[![Project Version][version-image]][version-url]
+[![Frontend][Frontend-image]][Frontend-url]
+[![Backend][Backend-image]][Backend-url]
+
+> Projeto de estudo
+
+Calculadora simples com as quatro operações básicas da matemática.
+
+---
+## Author
+
+**Roger de Azevedo** 
+
+* *Meu perfil profissional no* [LinkedIn][linkedin-url]
+
+## Showcase
+
+Esse projeto foi desenvolvimedo para demonstrar:
+
+* App em Adroid nativo
+* Programado na linguagem Kotlin
+* Lib para calcular expressoes matemáticas [net.objecthunter:exp4j:0.4.8]
+
+## Built With
+
+* [Gradle](https://gradle.org/) - Dependency Management
+
+## Release History
+
+* 0.0.1
+    * Initial work
+
+## Expressão númerica
+
+**1º Sinais de associação**
+
+* 1º - () Parênteses;
+* 2º - [] Colchetes;
+* 3º - {} Chaves;
+
+**2º Operações**
+
+* 1º - Potência e/ou raiz;
+* 2º - X e/ou / na ordem que aparece (da esquerda para direita)
+* 3º - + e/ou - na ordem que aparece (da esquerda para direita)
+
+Exemplo
+
+```
+8 / 2 (2 + 2) = ?
+8 / 2 * 4                    * Resolve primeiro o Sinail de associação de () Parênteses
+4 * 4                        * Resolve operador de Divisão / pq aparece primeiro
+16
+```
+
+---
+
+## Contributing
+
+1. Fork it (<https://github.com/yourname/yourproject/fork>)
+2. Create your feature branch (`git checkout -b feature/fooBar`)
+3. Commit your changes (`git commit -am 'Add some fooBar'`)
+4. Push to the branch (`git push origin feature/fooBar`)
+5. Create a new Pull Request
+
+<!-- Markdown link & img dfn's -->
+
+[header-url]: github-template.png
+[header-link]: https://github.com/alexandrerosseto
+
+[repository-url]: https://github.com/alexandrerosseto/wbshopping
+
+[cloud-provider-url]: https://wbshopping.herokuapp.com
+
+[linkedin-url]: https://www.linkedin.com/in/alexandrerosseto
+
+[wiki]: https://github.com/yourname/yourproject/wiki
+
+[version-image]: https://img.shields.io/badge/Version-1.0.0-brightgreen?style=for-the-badge&logo=appveyor
+[version-url]: https://img.shields.io/badge/version-1.0.0-green
+[Frontend-image]: https://img.shields.io/badge/Frontend-Android-blue?style=for-the-badge
+[Frontend-url]: https://img.shields.io/badge/Frontend-Android-blue?style=for-the-badge
+[Backend-image]: https://img.shields.io/badge/Backend-Kotlin-important?style=for-the-badge
+[Backend-url]: https://img.shields.io/badge/Backend-Kotlin-important?style=for-the-badge
---
+++ app/src/main/java/br/com/rogerfdeazevedo/calculadora_kotlin_app/MainActivity.kt
@@ -0,0 +1,112 @@
+package br.com.rogerfdeazevedo.calculadora_kotlin_app
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import net.objecthunter.exp4j.ExpressionBuilder
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        supportActionBar!!.hide()
+
+        btn_num_0.setOnClickListener{ setExpressao("0", true)}
+        btn_num_1.setOnClickListener{ setExpressao("1", true)}
+        btn_num_2.setOnClickListener{ setExpressao("2", true)}
+        btn_num_3.setOnClickListener{ setExpressao("3", true)}
+        btn_num_4.setOnClickListener{ setExpressao("4", true)}
+        btn_num_5.setOnClickListener{ setExpressao("5", true)}
+        btn_num_6.setOnClickListener{ setExpressao("6", true)}
+        btn_num_7.setOnClickListener{ setExpressao("7", true)}
+        btn_num_8.setOnClickListener{ setExpressao("8", true)}
+        btn_num_9.setOnClickListener{ setExpressao("9", true)}
+
+        btn_ponto.setOnClickListener{ setExpressao(".", true)}
+
+        btn_operador_adicao.setOnClickListener{ setExpressao("+", false)}
+        btn_operador_subtracao.setOnClickListener{ setExpressao("-", false)}
+        btn_operador_multiplicacao.setOnClickListener{ setExpressao("x", false)}
+        btn_operador_divisao.setOnClickListener{ setExpressao("/", false)}
+
+        btn_limpar.setOnClickListener {
+            txtView_expressao.text = ""
+            txtView_resultado.text = ""
+        }
+
+        btn_backspace.setOnClickListener {
+            val str = txtView_expressao.text.toString()
+
+            if(str.isNotBlank()){
+                txtView_expressao.text = str.substring(0, str.length - 1)
+            }
+
+            txtView_resultado.text = ""
+
+        }
+
+        btn_operador_igual.setOnClickListener{
+
+            try {
+
+                val expressao = ExpressionBuilder(txtView_expressao.text.toString().replace("x","*")).build()
+                val resultado = expressao.evaluate()
+                val longResultado = resultado.toLong()
+
+                if(resultado == longResultado.toDouble()){
+                    txtView_resultado.text = longResultado.toString()
+                } else {
+                    txtView_resultado.text = resultado.toString()
+                }
+
+            } catch (e: Exception) {
+                print(e.message)
+            }
+
+        }
+
+    }
+
+    fun setExpressao(numero: String, limparExpressao: Boolean) {
+
+        if(txtView_resultado.text.isNotEmpty()){
+            txtView_expressao.text = ""
+        }
+
+        if(limparExpressao){
+            txtView_resultado.text = ""
+            txtView_expressao.append(numero)
+        } else {
+            txtView_expressao.append(txtView_resultado.text)
+            txtView_expressao.append(numero)
+            txtView_resultado.text = ""
+        }
+
+    }
+
+    /**
+     *  Expressão númerica
+     *
+     *  1º Sinais de associação
+     *
+     *  1º - () Parênteses;
+     *  2º - [] Colchetes;
+     *  3º - {} Chaves;
+     *
+     *  2º Operações
+     *
+     *  1º - Potência e/ou raiz;
+     *  2º - X e/ou / na ordem que aparece (da esquerda para direita)
+     *  3º - + e/ou - na ordem que aparece (da esquerda para direita)
+     *
+     * Exemplo
+     *
+     * 8 / 2 (2 + 2) = ?
+     * 8 / 2 * 4                    * Resolve primeiro o Sinail de associação de () Parênteses
+     * 4 * 4                        * Resolve operador de Divisão / pq aparece primeiro
+     * 16
+     *
+     * **/
+
+}
